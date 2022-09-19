011a54a82da361df8db17813f33e703e5a0a5654
John Wilson <john256omar@gmail.com>
2022-09-19 18:25:38 +0100

    created operate function

--- scripts.js
+++ scripts.js
@@ -6,3 +6,14 @@ const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
 
+function operate(operator, num1, num2) {
+    if (operator === "+" ) {
+        return add(num1,num2)
+    } else if(operator === "-" ){
+        return subtract(num1,num2)
+    } else if(operator === "*" ){
+        return multiply(num1,num2)
+    } else if(operator === "/" ){
+        return divide(num1,num2)
+    } 
+}
