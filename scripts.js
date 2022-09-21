cb1364c8579cf59e317b978f6bdfca89f40ce18a
John Wilson <john256omar@gmail.com>
2022-09-21 21:05:53 +0100

    changed the way value is displayed to screen

--- scripts.js
+++ scripts.js
@@ -2,8 +2,34 @@ let operator = '';
 let previousValue = '';
 let currentValue = '';
 
+document.addEventListener("DOMContentLoaded", function() {
 
-const add = (num1, num2) => num1 + num2;
+    const clear = document.querySelector('.clear-button')
+    const equal = document.querySelector('.equal-button')
+
+    const previousScreen = document.querySelector('.previous')
+    const currentScreen = document.querySelector('.current')
+
+    const operators = document.querySelectorAll('.op-button')
+    const numbers = document.querySelectorAll('.num-button')
+
+    numbers.forEach(number => {
+        number.addEventListener('click', (e) =>{
+            handleNumber(e.target.textContent)
+            currentScreen.textContent = currentValue;
+          })
+       }) 
+
+})
+
+function handleNumber(num) {
+    currentValue += num;
+}
+
+
+
+ 
+/*const add = (num1, num2) => num1 + num2;
 
 const subtract = (num1, num2) => num1 - num2;
 
@@ -11,7 +37,7 @@ const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
 
-const operate = (num1,operator, num2) => {
+const operate = (operator,num1, num2) => {
     if (operator === "+" ) {
         return add(num1,num2)
     } else if(operator === "-" ){
@@ -21,18 +47,7 @@ const operate = (num1,operator, num2) => {
     } else if(operator === "/" ){
         return divide(num1,num2)
     } 
-}
-
-
-const display = document.querySelector('.display')
-const buttons = document.querySelectorAll('.num-button')
-
-buttons.forEach(button => {
-    button.addEventListener('click', () =>{
-    document.querySelector('.display').textContent += button.innerHTML
-      })
-   }) 
-
+}*/
 
  /*const display = document.querySelector('.display');
 
@@ -41,7 +56,6 @@ function populateDisplay(choice){
     display.innerText += number;
 
 }
-
 
 function numOperate(choiceOp) {
     const operator = choiceOp;
