ab1cbdeb4c25346f50bd1504380174c9d98d5940
John Wilson <john256omar@gmail.com>
2022-09-27 16:05:12 +0100

    Added comments

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ let operator = '';
 let previousValue = '';
 let currentValue = '';
 
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const add = (num1, num2) => num1 + num2; // Function that adds two arguments 
+const subtract = (num1, num2) => num1 - num2; // Function that subtracts two arguments 
+const multiply = (num1, num2) => num1 * num2; // Function that multiplies two arguments 
+const divide = (num1, num2) => num1 / num2; // Function that divides two arguments 
 
 document.addEventListener("DOMContentLoaded", function() {
     // Store all components on HTML in our JS
@@ -18,9 +18,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const operators = document.querySelectorAll('.op-button')
     const numbers = document.querySelectorAll('.num-button')
 
-    numbers.forEach((number) => number.addEventListener('click', (e) =>{
-            handleNumber(e.target.textContent)
-            currentScreen.textContent = currentValue;
+    numbers.forEach((number) => number.addEventListener('click', (e) =>{ //for each loop adds event listener to each div in array
+            handleNumber(e.target.textContent) // called function will target text content of div through the event 
+            currentScreen.textContent = currentValue; // the current displayed calc screen will display the value of currentValue 
           }))
 
     operators.forEach((op) => op.addEventListener("click", (e) =>{
@@ -40,25 +40,25 @@ document.addEventListener("DOMContentLoaded", function() {
     equal.addEventListener("click", ()=> { 
       calculate()
       previousScreen.textContent = '';
-      currentScreen.textContent = previousValue;
+      currentScreen.textContent = previousValue; // This causes the previousScreen to be blank and for the CurrentScreen to display the calculation 
     })
 })
 
-function handleNumber(num) {
+function handleNumber(num) { //function will add the argument passed to it onto the variable currentValue 
     if (currentValue.length <= 8){
     currentValue += num;
     }
 }
 
-function handleOperator(op) {
-    operator = op;
-    previousValue = currentValue;
-    currentValue = ''
+function handleOperator(op) { 
+    operator = op; // function will add the argument passed to it onto the variable operator 
+    previousValue = currentValue; // when an operator button is selected the variable previousValue will now have the value of currentValue 
+    currentValue = '' // current value will now have no value 
 }
 
 function calculate() {
     previousValue = Number(previousValue);  
-    currentValue = Number(currentValue); // converts value into number 
+    currentValue = Number(currentValue); // converts String value into number 
     
     if (operator === '+') {
      previousValue = add(previousValue, currentValue)
@@ -76,46 +76,8 @@ function calculate() {
     currentValue = previousValue.toString();
 }
 
-function roundNumber(num){
+function roundNumber(num){ //function will round the number to a whole number 
     return Math.round(num * 1000) / 1000;
 } 
 
 
-
-
-
- 
-/*const add = (num1, num2) => num1 + num2;
-
-const subtract = (num1, num2) => num1 - num2;
-
-const multiply = (num1, num2) => num1 * num2;
-
-const divide = (num1, num2) => num1 / num2;
-
-const operate = (operator,num1, num2) => {
-    if (operator === "+" ) {
-        return add(num1,num2)
-    } else if(operator === "-" ){
-        return subtract(num1,num2)
-    } else if(operator === "*" ){
-        return multiply(num1,num2)
-    } else if(operator === "/" ){
-        return divide(num1,num2)
-    } 
-}*/
-
- /*const display = document.querySelector('.display');
-
-function populateDisplay(choice){
-    const number = choice;
-    display.innerText += number;
-
-}
-
-function numOperate(choiceOp) {
-    const operator = choiceOp;
-    if (operator == 'plus') {
-        const firstDisplay = display.innerText;
-    }
-} */
